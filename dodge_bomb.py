d5c5bd6ed21c775e1dc71a24dae5e7b9d2243f37
Shogo <c0a231346d@edu.teu.ac.jp>
2024-04-23 15:05:26 +0900

    練習3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -11,20 +11,29 @@ derta = {pg.K_UP:(0,-5),
          pg.K_RIGHT:(+5,0)}
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+def check_bound(obj_rct:pg.Rect) -> tuple[bool,bool]:
+    yoko,tate = True,True
+    if obj_rct.left < 0 or WIDTH < obj_rct.right: 
+        yoko = False
+    if obj_rct.top < 0 or HEIGHT < obj_rct.bottom:
+        tate = False
+    return yoko,tate
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
+    # ここからこうかとんの設定
     bg_img = pg.image.load("fig/pg_bg.jpg")    
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
+    #ここから爆弾の設定
     bd_image = pg.Surface((20,20))
     bd_image.set_colorkey((0,0,0))
     pg.draw.circle(bd_image,(255,0,0),(10,10),10)
     bd_rct = bd_image.get_rect()
     bd_rct.center = random.randint(0,WIDTH),random.randint(0,HEIGHT)
-    vx,vy = +5,+5
+    vx,vy = +5,+5 #横方向速度、縦方向速度
     clock = pg.time.Clock()
     tmr = 0
     
@@ -34,7 +43,7 @@ def main():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
-
+        # こうかとんの移動と表示
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         for k,v in derta.items():
@@ -50,9 +59,17 @@ def main():
         #if key_lst[pg.K_RIGHT]:
             #sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True,True):
+            kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
         screen.blit(kk_img, kk_rct)
+        #爆弾の移動と表示
         bd_rct.move_ip(vx,vy)
         screen.blit(bd_image,bd_rct)
+        yoko,tate = check_bound(bd_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
         pg.display.update()
         tmr += 1
         clock.tick(50)
